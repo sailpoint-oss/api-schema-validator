1c799d6b941c4cec2857489e2db4ec92de695b1a
colin-mckibben-sp <coln.mckibben@sailpoint.com>
2022-07-14 12:03:41 -0400

    Made the speccy resolve command quiet

--- workflow-validate.sh
+++ workflow-validate.sh
@@ -36,8 +36,8 @@ file_path_length () {
 }
 
 # Build the API spec
-speccy resolve ../cloud-api-client-common/api-specs/src/main/yaml/sailpoint-api.v3.yaml -o v3.yaml
-speccy resolve ../cloud-api-client-common/api-specs/src/main/yaml/sailpoint-api.beta.yaml -o beta.yaml
+speccy resolve --quiet ../cloud-api-client-common/api-specs/src/main/yaml/sailpoint-api.v3.yaml -o v3.yaml
+speccy resolve --quiet ../cloud-api-client-common/api-specs/src/main/yaml/sailpoint-api.beta.yaml -o beta.yaml
 
 cd ../cloud-api-client-common
 BASE_DIR="api-specs/src/main/yaml"
