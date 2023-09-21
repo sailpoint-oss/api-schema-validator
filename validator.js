9a5a18fa295d63ee4a4951249ef284730aa169a9
Colin McKibben <colin.mckibben@sailpoint.com>
2023-09-21 11:35:16 -0400

    Validate eq, co, and in

--- validator.js
+++ validator.js
@@ -8,6 +8,44 @@ const dotenv = require("dotenv");
 const yargs = require('yargs');
 const YAML = require('yamljs');
 
+
+// Find all top level attributes within the response schema that are not arrays or objects
+// Assign the correct subset of operators that are applicable to each type of property.
+// TODO: Support arrays and objects
+function getFilterableProperties(schema) {
+    let filterableProperties = {}
+    // Since filters only work for list endpoints, the schema will always be an array of items.
+    // Dive into the array to get the schema.
+    for (const [property, propertySchema] of Object.entries(schema['items']['properties'])) {
+        if (propertySchema.type === 'string') {
+            filterableProperties[property] = {
+                type: propertySchema.type,
+                operators: ['co', 'eq', 'ge', 'gt', 'in','le', 'lt', 'ne', 'pr', 'sw'],
+                supported: [],
+                unsupported: []
+            }
+        } else if (propertySchema.type === 'boolean') {
+            filterableProperties[property] = {
+                type: propertySchema.type,
+                operators: ['eq', 'ne', 'pr'],
+                supported: [],
+                unsupported: []
+            }
+        } else if (propertySchema.type === 'number') {
+            filterableProperties[property] = {
+                type: propertySchema.type,
+                operators: ['eq', 'ne', 'pr', 'gt', 'ge', 'lt', 'le'],
+                supported: [],
+                unsupported: []
+            }
+        } else {
+            console.debug("objects and arrays not yet supported")
+        }
+    }
+
+    return filterableProperties;
+}
+
 function getArgs() {
     const argv = yargs
         .option('input', {
@@ -81,30 +119,189 @@ function getArgs() {
     return argv;
 }
 
+function parseFilters(description) {
+    const filters = {};
+    const lines = description.split("\n");
+    const attributeLines = lines.filter(line => line.includes("**:"));
+    attributeLines.forEach(line => {
+        const attOpSplit = line.replaceAll("*", "").split(":");
+        const attribute = attOpSplit[0].trim();
+        const opSplit = attOpSplit[1].trim().split(",");
+        const operators = opSplit.map(op => op.trim());
+        filters[attribute] = operators;
+    }) 
+    return filters;
+}
+
+function handleResError(error) {
+    if (error) {
+        const method = error.response.request.method;
+        const endpoint = error.response.request.path;
+        console.log(`${method} ${endpoint}`);
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+        }
+    } else {
+        console.log(`A serious error occurred for ${path}`);
+    }
+}
+
+async function testCo(httpClient, controlRes, property, path, propertiesToTest) {
+    const example = controlRes.data.filter(item => property in item)[0][property]
+    if (typeof example === "string") {
+        const partial = example.substring(0, example.length - 1)
+        const res = await httpClient.get(path, { params: { filters: `${property} co "${partial}"`}})
+        const badMatches = res.data.filter(item => item[property] !== example)
+        if (badMatches.length > 0) {
+            propertiesToTest[property].unsupported.push('co')
+        } else {
+            propertiesToTest[property].supported.push('co')
+        }
+    } else {
+        propertiesToTest[property].unsupported.push('co')
+    } 
+}
+
+async function testEq(httpClient, controlRes, property, path, propertiesToTest) {
+    const example = controlRes.data.filter(item => property in item)[0][property]
+
+    let res = undefined
+    if (typeof example === "string") {
+        res = await httpClient.get(path, { params: { filters: `${property} eq "${example}"`}})
+    } else {
+        res = await httpClient.get(path, { params: { filters: `${property} eq ${example}`}})
+    }
+
+    const badMatches = res.data.filter(item => item[property] !== example)
+    if (badMatches.length > 0) {
+        propertiesToTest[property].unsupported.push('eq')
+    } else {
+        propertiesToTest[property].supported.push('eq')
+    }
+}
+
+async function testIn(httpClient, controlRes, property, path, propertiesToTest) {
+    const example = controlRes.data.filter(item => property in item)[0][property]
+
+    let res = undefined
+    if (typeof example === "string") {
+        res = await httpClient.get(path, { params: { filters: `${property} in ("${example}")`}})
+    } else {
+        res = await httpClient.get(path, { params: { filters: `${property} in (${example})`}})
+    }
+
+    const badMatches = res.data.filter(item => item[property] !== example)
+    if (badMatches.length > 0) {
+        propertiesToTest[property].unsupported.push('in')
+    } else {
+        propertiesToTest[property].supported.push('in')
+    }
+}
+
+async function testFilters(httpClient, path, propertiesToTest) {
+    // Invoke the path without any filters so we have test data to work with when crafting the queries
+    const controlRes = await httpClient.get(path).catch(error => {handleResError(error)});
+
+    for (const [property, value] of Object.entries(propertiesToTest)) {
+        for (const operation of value.operators) {
+            switch(operation) {
+                case 'co':
+                    try {
+                        await testCo(httpClient, controlRes, property, path, propertiesToTest);
+                    } catch (error) {
+                        propertiesToTest[property].unsupported.push('co')
+                    }
+                    break;
+                case 'eq':
+                    try {
+                        await testEq(httpClient, controlRes, property, path, propertiesToTest)
+                    } catch (error) {
+                        propertiesToTest[property].unsupported.push('eq')
+                    }
+                    break;
+                case 'in':
+                    try {
+                        await testIn(httpClient, controlRes, property, path, propertiesToTest)
+                    } catch (error) {
+                        propertiesToTest[property].unsupported.push('in')
+                    }
+                    break;
+            }
+        }
+    } 
+
+    return propertiesToTest
+}
+
+async function validateFilters(httpClient, method, path, spec) {
+    let uniqueErrors = {
+        method: method,
+        endpoint: path,
+        errors: {
+            undocumentedFilters: [],
+            unsupportedFilters: []
+        }
+    };
+    let documentedFilters = null;
+
+    if (spec.paths[path].get.parameters != undefined) {
+        const filteredParams = spec.paths[path].get.parameters.filter(param => param.name === "filters")
+        const schema = spec.paths[path].get.responses['200'].content['application/json'].schema;
+        if (filteredParams.length == 1) {
+            try {
+                documentedFilters = parseFilters(filteredParams[0].description);
+                const propertiesToTest = getFilterableProperties(schema)
+                const testedProperties = await testFilters(httpClient, path, propertiesToTest)
+                for (property in testedProperties) {
+                    if (property in documentedFilters) {
+                        for (supportedFilter of testedProperties[property]["supported"]) {
+                            // A supported filter is not documented
+                            if (!(documentedFilters[property].includes(supportedFilter))) {
+                                uniqueErrors.errors['undocumentedFilters'].push({
+                                    'message': `The property ${property} supports "${supportedFilter}" but it is not documented.`,
+                                    'data': null
+                                })
+                            }
+                        }
+                        for (documentedFilter of documentedFilters[property]) {
+                            // A documented filter is not supported
+                            if (!(testedProperties[property]["supported"].includes(documentedFilter))) {
+                                uniqueErrors.errors['unsupportedFilters'].push({
+                                    'message': `The property ${property} does not support "${documentedFilter}" but the documentation says it does.`,
+                                    'data': null
+                                })
+                            }
+                        }
+                    } else {
+
+                    }
+                }
+            } catch (error) {
+                uniqueErrors.errors['Invalid Filters'] = {
+                    'message': `Unable to parse the filters due to improper format: ${error.message}`,
+                    'data': null
+                }
+                return uniqueErrors;
+            }
+        }
+    }
+
+    return uniqueErrors;
+}
+
 async function validateSchema(httpClient, ajv, path, spec) {
     const schema = spec.paths[path].get.responses['200'].content['application/json'].schema;
-    res = await httpClient.get(path).catch(error => {
-        if (error) {
-            const method = error.response.request.method;
-            const endpoint = error.response.request.path;
-            console.log(`${method} ${endpoint}`);
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-            } else if (error.request) {
-                // The request was made but no response was received
-                console.log(error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log('Error', error.message);
-            }
-        } else {
-            console.log(`A serious error occurred for ${path}`);
-        }
-    });
+    res = await httpClient.get(path).catch(error => {handleResError(error)});
 
     if (res) {
         uniqueErrors = {
@@ -157,8 +354,9 @@ async function validatePath(httpClient, ajv, path, spec) {
     if ("get" in spec.paths[path] && !path.includes('{')) {
         const contentType = spec.paths[path].get.responses['200'].content;
         if ("application/json" in contentType) {
-            const schemaErrors = await validateSchema(httpClient, ajv, path, spec);
-            return schemaErrors;
+            //const schemaErrors = await validateSchema(httpClient, ajv, path, spec);
+            const filterErrors = validateFilters(httpClient, "get", path, spec);
+            return null;
         } else {
             console.log(`Path ${path} uses ${JSON.stringify(contentType)} instead of application/json.  Skipping.`);
         }
