af34861ac7b4ba32c39f8946c78a10bd84d20e41
Colin McKibben <colin.mckibben@sailpoint.com>
2023-11-02 14:22:19 -0400

    Added double quotes around the echo statement to preserve new line characters

--- workflow-validate.sh
+++ workflow-validate.sh
@@ -55,7 +55,7 @@ do
         echo "**${CHANGED_FILE}** is used in one or more paths that have an invalid schema.  Please fix the schema validation issues below.  For more information on this PR check, please see the [API schema validator README](https://github.com/sailpoint/cloud-api-client-common#api-schema-validator).  For a list of common error messages and how to fix them, please [see this section in the README](https://github.com/sailpoint/cloud-api-client-common#common-api-validator-errors)."
         echo "| Path | Errors |"
         echo "|-|-|"
-        echo $VALIDATION
+        echo "$VALIDATION"
         echo "---"
     fi
 done
