b09012d6590ebfcb2cfc2a4b2160b596fff06f0f
Colin McKibben <colin.mckibben@sailpoint.com>
2024-04-26 09:47:03 -0400

    Reverted validate-master

--- validate-master.sh
+++ validate-master.sh
@@ -12,8 +12,8 @@ git clone git@github.com:sailpoint/cloud-api-client-common.git
 speccy resolve --quiet cloud-api-client-common/api-specs/src/main/yaml/sailpoint-api.v3.yaml -o v3.yaml
 speccy resolve --quiet cloud-api-client-common/api-specs/src/main/yaml/sailpoint-api.beta.yaml -o beta.yaml
 
-node validator.js -i v3.yaml | tee v3-results.txt
-node validator.js -i beta.yaml | tee beta-results.txt
+node validator.js -i v3.yaml --skip-filters --skip-sorters | tee v3-results.txt
+node validator.js -i beta.yaml --skip-filters --skip-sorters | tee beta-results.txt
 
 rm -rf cloud-api-client-common
 rm v3.yaml
